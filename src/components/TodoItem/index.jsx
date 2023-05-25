fcc3aff95e6180a3dc0d4cd3235ec6a2ee7477f9
Adnan Ali <adnanali6688@gmail.com>
2023-05-25 23:38:56 +0200

    added styles functionality

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,20 @@
 import styles from './TodoItem.module.scss'
+import { useState } from 'react'
 
 const TodoItem = (props) => {
     const item = props.list
     const { id, name } = item
+
+    const [isChecked, setIsChecked] = useState(false)
+
+    const markHandler = () => {
+        setIsChecked(!isChecked)
+    }
+
     return <>
       <div className={styles.mainWrapper}>
         <div className={styles.content}>
-            <p>{name}</p>
+            <p onClick={markHandler} className={`${isChecked ? styles.line : ''}`} >{name}</p>
         </div>
         {/* <button className={`${styles.btn} ${styles.btnEdit}`}>Edit</button> */}
         <button className={`${styles.btn} ${styles.btnDelete}`}>Delete</button>
